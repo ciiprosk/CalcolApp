8738855cac7c7e3049bccf7b129d347fde89af4d
Antonio Vitale <a.vitale132@studenti.unisa.it>
2026-03-31 09:18:01 +0200

    messo metodi da implementare

--- src/main/java/it/unisa/calculator/Calculcator.java
+++ src/main/java/it/unisa/calculator/Calculcator.java
@@ -1,11 +1,37 @@
 package it.unisa.calculator;
 
 public class Calculcator {
-    private int firstNumber;
-    private int secondNumber;
+    private float firstNumber;
+    private float secondNumber;
+    private float lastResult;
+
 
     public Calculcator(int firstNumber, int secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
+    }
+
+    public float sum (float firstNumber, float secondNumber) {
+        //toDO
+    }
+
+    public float subtract (float firstNumber, float secondNumber) {
+        //toDO
+    }
+
+    public float multiply (float firstNumber, float secondNumber) {
+        //toDO
+    }
+
+    public float divide (float firstNumber, float secondNumber) {
+        //toDO
+    }
+
+    public float power (float firstNumber, float secondNumber) {
+        //toDO
+    }
+
+    public void result (float lastResult){
+        //toDo
     }
 }
