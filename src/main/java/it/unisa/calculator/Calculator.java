92b2501fba0af38c1693a18c4edb3f0e4a0440ab
ciiprosk <rosapassaro28@gmail.com>
2026-03-31 10:03:22 +0200

    implemngtazione coda cronogogia

--- src/main/java/it/unisa/calculator/Calculator.java
+++ src/main/java/it/unisa/calculator/Calculator.java
@@ -1,18 +1,22 @@
-
 package it.unisa.calculator;
 
-public class Calculator {
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Calculator{
     private float firstNumber;
     private float secondNumber;
     private float lastResult;
+    private Queue<Float> history;
 
 
     public Calculator() {
-
+        history = new ArrayDeque<>(10);
     }
     public Calculator(int firstNumber, int secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
+        history = new ArrayDeque<>(10);
     }
 
     public float getLastResult() {
@@ -38,11 +42,14 @@ public class Calculator {
     }
 
     public void setLastResult(float lastResult) {
+
         this.lastResult = lastResult;
+        history.add(lastResult);
     }
 
     public float sum (float firstNumber, float secondNumber) {
        setLastResult(firstNumber + secondNumber);
+
         return lastResult;
     }
 
