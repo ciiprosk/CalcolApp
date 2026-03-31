b72925b6891c897db23f5e31c787094935e93d54
ciiprosk <rosapassaro28@gmail.com>
2026-03-31 09:24:56 +0200

    operazioni completate

--- src/main/java/it/unisa/calculator/Calculcator.java
+++ src/main/java/it/unisa/calculator/Calculcator.java
@@ -1,3 +1,4 @@
+
 package it.unisa.calculator;
 
 public class Calculcator {
@@ -12,26 +13,43 @@ public class Calculcator {
     }
 
     public float sum (float firstNumber, float secondNumber) {
-        //toDO
+        lastResult = firstNumber + secondNumber;
+        return lastResult;
     }
 
     public float subtract (float firstNumber, float secondNumber) {
-        //toDO
+        lastResult = firstNumber - secondNumber;
+        return lastResult;
     }
 
     public float multiply (float firstNumber, float secondNumber) {
-        //toDO
+        lastResult= firstNumber * secondNumber;
+        return lastResult;
     }
 
     public float divide (float firstNumber, float secondNumber) {
-        //toDO
+        lastResult= firstNumber / secondNumber;
+        return lastResult;
     }
 
     public float power (float firstNumber, float secondNumber) {
-        //toDO
+        lastResult= (float) Math.pow(firstNumber, secondNumber);
+        return lastResult;
     }
 
-    public void result (float lastResult){
-        //toDo
+    public float sin (float firstNumber) {
+        lastResult= (float) Math.sin(firstNumber);
+        return lastResult;
     }
+
+    public float cos (float firstNumber) {
+        lastResult= (float) Math.cos(firstNumber);
+        return lastResult;
+    }
+
+    public float tan (float firstNumber) {
+        lastResult= (float) Math.tan(firstNumber);
+        return lastResult;
+    }
+
 }
