3c8789eb42f56a43c8c6dbba3b0fa56b19da7b03
Antonio Vitale <a.vitale132@studenti.unisa.it>
2026-03-31 09:13:14 +0200

    costruttore

--- src/main/java/it/unisa/calculator/Calculcator.java
+++ src/main/java/it/unisa/calculator/Calculcator.java
@@ -1,4 +1,11 @@
 package it.unisa.calculator;
 
 public class Calculcator {
+    private int firstNumber;
+    private int secondNumber;
+
+    public Calculcator(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
 }
